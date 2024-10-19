6265b4831417da0e7e7cbb21205e90d7b863189a
zakaria-lagouader <zakaria.lagouader@gmail.com>
2024-10-19 12:51:55 +0100

    added routing

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Page from "@/app/dashboard/page.tsx";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
 import "./index.css";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Page />
+		<RouterProvider router={router} />
 	</StrictMode>
 );
