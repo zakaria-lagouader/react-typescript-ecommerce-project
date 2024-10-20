154d0148a3ec9232acc1cbdd0fb9d1cc57fde2c5
zakaria-lagouader <zakaria.lagouader@gmail.com>
2024-10-20 21:48:02 +0100

    added create product page

--- admin/src/routes/admin/auth/register.tsx
+++ admin/src/routes/admin/auth/register.tsx
@@ -1,68 +1,68 @@
-import { AuthLayout } from '@/components/auth-layout'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { AuthLayout } from "@/components/auth-layout";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/admin/auth/register')({
-  component: Page,
-})
+export const Route = createFileRoute("/admin/auth/register")({
+	component: Page,
+});
 
 function Page() {
-  return (
-    <AuthLayout>
-      <div className="mx-auto grid w-[350px] gap-6">
-        <div className="grid gap-2 text-center">
-          <h1 className="text-3xl font-bold">Register</h1>
-          <p className="text-balance text-muted-foreground">
-            Enter your info below to create your account
-          </p>
-        </div>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              id="name"
-              name="name"
-              type="text"
-              placeholder="Johne Doe"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" name="password" type="password" required />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password_confirmation">Confirm password</Label>
-            <Input
-              id="password_confirmation"
-              name="password_confirmation"
-              type="password"
-              required
-            />
-          </div>
-          <Button type="submit" className="w-full">
-            Register
-          </Button>
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Already have an account?{' '}
-          <Link to="/admin/login" className="underline">
-            Sign in
-          </Link>
-        </div>
-      </div>
-    </AuthLayout>
-  )
+	return (
+		<AuthLayout>
+			<div className="mx-auto grid w-[350px] gap-6">
+				<div className="grid gap-2 text-center">
+					<h1 className="text-3xl font-bold">Register</h1>
+					<p className="text-balance text-muted-foreground">
+						Enter your info below to create your account
+					</p>
+				</div>
+				<div className="grid gap-4">
+					<div className="grid gap-2">
+						<Label htmlFor="name">Name</Label>
+						<Input
+							id="name"
+							name="name"
+							type="text"
+							placeholder="Johne Doe"
+							required
+						/>
+					</div>
+					<div className="grid gap-2">
+						<Label htmlFor="email">Email</Label>
+						<Input
+							id="email"
+							name="email"
+							type="email"
+							placeholder="m@example.com"
+							required
+						/>
+					</div>
+					<div className="grid gap-2">
+						<Label htmlFor="password">Password</Label>
+						<Input id="password" name="password" type="password" required />
+					</div>
+					<div className="grid gap-2">
+						<Label htmlFor="password_confirmation">Confirm password</Label>
+						<Input
+							id="password_confirmation"
+							name="password_confirmation"
+							type="password"
+							required
+						/>
+					</div>
+					<Button type="submit" className="w-full">
+						Register
+					</Button>
+				</div>
+				<div className="mt-4 text-center text-sm">
+					Already have an account?{" "}
+					<Link to="/admin/auth/login" className="underline">
+						Sign in
+					</Link>
+				</div>
+			</div>
+		</AuthLayout>
+	);
 }
